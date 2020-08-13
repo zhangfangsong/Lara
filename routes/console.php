0b16e89ac3a09893720ac59dc88aaf48c69c2865
fangsong.zhang <845573796@qq.com>
2020-08-13 21:58:40 +0800

    自定义闭包命令

--- routes/console.php
+++ routes/console.php
@@ -1,18 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
+Artisan::command('about:lara', function () {
+    $this->info("Lara是一款基于Laravel框架6.x版本开发的个人博客系统，它灵活轻巧，简单易用并易于扩展，适合搭建个人博客或作为企业 CMS 使用。");
+})->describe('显示 Lara 博客系统简介');
