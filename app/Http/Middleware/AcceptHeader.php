90a655821a1b60cd149937954d1813ae5ca06708
fangsong.zhang <845573796@qq.com>
2020-08-17 23:39:32 +0800

    Api基础

---
+++ app/Http/Middleware/AcceptHeader.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AcceptHeader
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //调用默认返回json格式
+        $request->headers->set('Accept', 'application/json');
+
+        return $next($request);
+    }
+}
