0814fb7544a22ff7a60c73b5bdad42284ec0f7c3
fangsong.zhang <845573796@qq.com>
2020-08-25 14:28:34 +0800

    登录验证修改

--- app/Http/Requests/Api/AuthorizationRequest.php
+++ app/Http/Requests/Api/AuthorizationRequest.php
@@ -13,7 +13,7 @@ class AuthorizationRequest extends FormRequest
     {
         return [
             'username' => 'required|string',
-            'password' => 'required|alpha_dash|min:6',
+            'password' => 'required|string|min:6',
         ];
     }
 }
